e9086a7e79e3766ed13fcebc3f0d32731fa3ef4f
NGUYEN DAC HOANG DAT <datndhhe170720@fpt.edu.vn>
2024-03-23 10:53:57 +0700

    login

---
+++ Controllers/AccessController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using ProjectPRN221_2.Models;
+
+namespace ProjectPRN221_2.Controllers
+{
+    public class AccessController : Controller
+    {
+        private readonly TestProjectPRN221Context _context;
+
+        public AccessController(TestProjectPRN221Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Login()
+        {
+            ClaimsPrincipal claimUser = HttpContext.User;
+
+            if (claimUser.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(Account modelLogin)
+        {
+            var email = _context.Accounts.First().Account1;
+            var password = _context.Accounts.First().Password;
+
+            if (modelLogin.Account1 == email && modelLogin.Password == password)
+            {
+                List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier, modelLogin.Account1),
+                new Claim("OtherProperties", "Example Role")
+            };
+
+                ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                AuthenticationProperties properties = new AuthenticationProperties()
+                {
+                    AllowRefresh = true,
+                    //IsPersistent = modelLogin.KeepLoggedIn,
+                };
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), properties);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewData["ValidateMessage"] = "user not found";
+
+            return View();
+        }
+    }
+}
--- obj/Debug/net6.0/ProjectPRN221_2.AssemblyInfo.cs
+++ obj/Debug/net6.0/ProjectPRN221_2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectPRN221_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d13858fc0ecc42f84e5cafb201f9d4d44a7b685e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectPRN221_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectPRN221_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
